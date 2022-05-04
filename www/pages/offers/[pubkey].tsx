13cb80cae9cfa1e2bf2aa843bea2c31b8a347d19
Jacob Peddicord <jacob.peddicord@hey.com>
2022-05-04 11:12:11 -0400

    add decryption of shipping info

--- www/pages/offers/[pubkey].tsx
+++ www/pages/offers/[pubkey].tsx
@@ -2,13 +2,19 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { BigNumber, ethers } from 'ethers';
 import { SellOrder } from 'rwtp';
+import * as ethUtil from 'ethereumjs-util';
+import * as sigUtil from '@metamask/eth-sig-util';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import {
   CashIcon,
   ChevronRightIcon,
   InboxInIcon,
+  LockClosedIcon,
+  MailIcon,
+  HomeIcon
 } from '@heroicons/react/solid';
+import { json } from 'stream/consumers';
 
 interface Offer {
   buyer: string;
@@ -17,19 +23,21 @@ interface Offer {
   uri: string;
   state: string;
   acceptedAt: number;
+  email: string;
+  address: string;
 }
 
 const STATES = ["CLOSED", "OPEN", "COMMITTED", "COMPLETED"];
+
 
 function Inner() {
   const router = useRouter();
   const [offers, setOffers] = useState<Offer[]>();
 
   useEffect(() => {
-    if (!router.query.pubkey) return;
-
     // TODO: Handle when there are no offers
     async function load() {
+      if (!router.query.pubkey) return;
       // Prepare prepare provider
       const provider = new ethers.providers.Web3Provider(
         window.ethereum as any
@@ -64,7 +72,9 @@ function Inner() {
           stake: ((stake as BigNumber).toNumber() / Math.pow(10, decimals)).toString() || "",
           uri: uri,
           state: STATES[state],
-          acceptedAt: acceptedAt
+          acceptedAt: acceptedAt,
+          email: localStorage.getItem(`${uri}-email`) ?? "",
+          address: localStorage.getItem(`${uri}-address`) ?? ""
         }
       }));
       console.log(offers);
@@ -73,6 +83,53 @@ function Inner() {
     load().catch(console.error);
   }, [router.query.pubkey]);
 
+  async function decryptShippingData(index: number) {
+    if (!offers) return;
+
+    // Fetch encrypted data from IPFS
+    const uri = offers[index].uri;
+    const cid = (uri as string).replace("ipfs://", "");
+    const result = await fetch(`https://ipfs.io/ipfs/${cid}`, {
+      method: 'GET'
+    });
+    const data = await result.text()
+
+    // Decrypt data with metamask
+    const provider = new ethers.providers.Web3Provider(
+      window.ethereum as any
+    );
+    const signer = await provider.getSigner().getAddress();
+    const decrypted = await provider.send('eth_decrypt', [data, signer]);
+
+    // Update offers
+    const json = JSON.parse(decrypted);
+    const offer = offers[index];
+    offer.email = json["email"] ?? "";
+    localStorage.setItem(`${uri}-email`, offer.email);
+    offer.address = json["shippingAddress"] ?? "";
+    localStorage.setItem(`${uri}-address`, offer.address);
+    offers[index] = offer;
+    setOffers([...offers]);
+  }
+
+  async function commitToOffer(buyer: string) {
+    // TODO
+    // // Load sell order
+    // if (!router.query.pubkey) return;
+    // const provider = new ethers.providers.Web3Provider(
+    //   window.ethereum as any
+    // );
+    // const signer = provider.getSigner();
+    // const sellOrder = new ethers.Contract(
+    //   router.query.pubkey as string,
+    //   SellOrder.abi,
+    //   signer
+    // );
+    // // Approve ERC 20 transfer for stake
+    // const tx = await sellOrder.commit(buyer);
+    // console.log(tx);
+  }
+
   if (!offers) {
     return (
       <div className="max-w-4xl mx-auto flex flex-col pb-24">Loading...</div>
@@ -80,7 +137,7 @@ function Inner() {
   }
 
   return (
-    <div className="bg-blue-50 h-full">
+    <div className="bg-blue-50">
       <div className="max-w-2xl mx-auto px-4 pt-24 w-full flex gap-4">
         <h1 className="text-4xl font-bold">Offers</h1>
         <a href="/" className="underline mt-auto">
@@ -88,14 +145,14 @@ function Inner() {
         </a>
       </div>
       <div className="max-w-2xl mx-auto my-auto flex flex-col pb-24 p-4 gap-4">
-        {offers.map((offer) => {
+        {offers.map((offer, index) => {
           return (<div hidden={offer.state === "CLOSED"} key={offer.buyer} className="bg-white border border-black">
             <div
               className="bg-gray-50 px-2 py-1 border-b font-mono text-sm"
             >
               <div className="opacity-50 text-xs">From: {offer.buyer}</div>
               <div className="flex items-center py-2">
-              <div className={offer.state === "OPEN" ? "text-blue-600 font-bold" : "opacity-50"}>Offer Made</div>
+                <div className={offer.state === "OPEN" ? "text-blue-600 font-bold" : "opacity-50"}>Offer Made</div>
 
                 <ChevronRightIcon className="h-4 w-4" />
 
@@ -122,13 +179,44 @@ function Inner() {
                   <span className="font-bold"> {offer.stake} USDC.</span>
                 </div>
               </div>
+
+              {offer.email.length == 0 && offer.address.length == 0 && <div className="flex items-center">
+                <LockClosedIcon className="h-4 w-4 mr-2" />
+                <div>
+                  <a href={"https://ipfs.io/ipfs/" + offer.uri.replace("ipfs://", "")} target="_blank" className="underline">Encrypted shipping data</a>
+                </div>
+              </div>}
+
+              {offer.email && offer.address && <div className="flex items-center">
+                <MailIcon className="h-4 w-4 mr-2" />
+                <div>
+                  {offer.email}
+                </div>
+              </div>}
+
+              {offer.email && offer.address && <div className="flex items-center">
+                <HomeIcon className="h-4 w-4 mr-2" />
+                <div>
+                  {offer.address}
+                </div>
+              </div>}
             </div>
-            <div className="px-4 pb-4">
-              <button className="rounded bg-blue-500 text-white border border-blue-700 px-4 py-2 text-sm disabled:opacity-50 transition-all">
+            <div className="flex flex-row gap-4 px-4 pb-4">
+              {offer.email.length == 0 && offer.address.length == 0 && <button
+                className="rounded bg-blue-500 text-white border border-blue-700 px-4 py-2 text-sm disabled:opacity-50 transition-all"
+                onClick={async () => await decryptShippingData(index)}
+              >
+                Decrypt data
+              </button>}
+              <button
+                className="rounded bg-blue-500 text-white border border-blue-700 px-4 py-2 text-sm disabled:opacity-50 transition-all"
+                onClick={async () => await commitToOffer(offer.buyer)}
+              >
                 Accept offer
               </button>
             </div>
-          </div>)}
+          </div>)
+        }
         )}
       </div>
     </div>
