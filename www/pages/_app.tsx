2909c026b3b4d70954eaf0ff47818366e71aa7e9
Evan Conrad <evan@roomservice.dev>
2022-05-04 08:54:35 -0700

    fix bluejeans

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 import '../styles/prism.css';
 import type { AppProps } from 'next/app';
 import { Provider, createClient } from 'wagmi';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const client = createClient();
 
@@ -9,6 +11,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider client={client}>
       <Component {...pageProps} />
+      <Script
+        src="https://bluejeans.rwtp.org/script.js"
+        data-site="CAQMFJOY"
+        defer
+      />
     </Provider>
   );
 }
