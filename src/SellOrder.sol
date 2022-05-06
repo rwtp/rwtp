33c242d8cb82aec1fa472044e70dcf18da7cf459
Evan Conrad <evan@roomservice.dev>
2022-05-06 13:40:14 -0700

    remove ownable

--- src/SellOrder.sol
+++ src/SellOrder.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
 import './interfaces/IOrderBook.sol';
 
 contract SellOrder {
