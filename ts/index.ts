4dc2e071c1e35bfec7c90467680760e1d91e447b
Evan Conrad <evan@roomservice.dev>
2022-05-06 13:32:17 -0700

    0.7.2

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,11 @@
 import SellOrder from '../out/SellOrder.sol/SellOrder.json';
-import OrderBook from '../out/OrderBook.sol/OrderBook.json';
+import OrderBookWithoutAddress from '../out/OrderBook.sol/OrderBook.json';
+
+const OrderBook = {
+  ...OrderBookWithoutAddress,
+
+  // The address of the deployed contract
+  address: '0x667d3ded6c891453e4b1bf6032cd0c22e0c31bac',
+};
 
 export { SellOrder, OrderBook };
