037297ef96d6f102a53ee4fc425047313469324d
Evan Conrad <evan@roomservice.dev>
2022-05-06 13:40:32 -0700

    rm unused import

--- src/SellOrder.sol
+++ src/SellOrder.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 import './interfaces/IOrderBook.sol';
 
 contract SellOrder {
