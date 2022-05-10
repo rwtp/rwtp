33d0413ca7fe5052e380aaebce57d905d7f092f6
Evan Conrad <evan@roomservice.dev>
2022-05-09 19:14:18 -0700

    wip

---
+++ www/lib/useSubgraph.ts
@@ -0,0 +1,18 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import cn from 'classnames';
+import { useContract, useProvider } from 'wagmi';
+import { OrderBook } from 'rwtp';
+import { useEffect } from 'react';
+import useSWR from 'swr';
+import { request } from 'graphql-request';
+
+const fetcher = (query: any, variables: any) =>
+  request(
+    'https://api.thegraph.com/subgraphs/name/chitalian/real-world-trade-protocol-rinkeby',
+    query,
+    variables
+  );
+
+export function useSubgraph(args: string | [string, any]) {
+  return useSWR(args, fetcher);
+}
