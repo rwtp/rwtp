f9567557f510b89ca3d659d82f3fa2b5c33da4a6
Evan Conrad <evan@roomservice.dev>
2022-05-10 16:54:22 -0700

    Add login on orders page

--- www/pages/app/orders/index.tsx
+++ www/pages/app/orders/index.tsx
@@ -1,5 +1,11 @@
+import {
+  FingerPrintIcon,
+  LightningBoltIcon,
+  SwitchHorizontalIcon,
+} from '@heroicons/react/solid';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import cn from 'classnames';
+import Image from 'next/image';
 import {
   SellOrderData,
   useSellOrder,
@@ -9,11 +15,81 @@ import {
 function Layout(props: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-full">
-      <div className="flex px-4 py-4 justify-between items-center w-full bg-gray-50 border-b">
+      <div className="flex px-4 pb-8 pt-8 justify-between items-center w-full">
+        <Image width={24} height={24} src="/transitionLogo.png" />
+
         <div>
-          <ConnectButton />
+          <ConnectButton.Custom>
+            {({
+              account,
+              mounted,
+              chain,
+              openConnectModal,
+              openChainModal,
+              openAccountModal,
+            }) => {
+              if (!mounted || !account || !chain) {
+                return (
+                  <button
+                    className="bg-white border text-sm border-black rounded px-2 py-1 flex items-center"
+                    onClick={openConnectModal}
+                  >
+                    Connect Wallet <FingerPrintIcon className="h-4 w-4 ml-2" />
+                  </button>
+                );
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <button
+                    className="bg-white border text-sm border-black rounded px-2 py-1 flex items-center"
+                    onClick={openChainModal}
+                  >
+                    Wrong Network{' '}
+                    <SwitchHorizontalIcon className="h-4 w-4 ml-2" />
+                  </button>
+                );
+              }
+
+              const keyDetails = (
+                <>
+                  <span className="text-gray-400">0x</span>
+                  {account.address.substring(2, 6)}
+                  <span className="text-gray-400">...</span>
+                  {account.address.substring(
+                    account.address.length - 4,
+                    account.address.length
+                  )}
+                </>
+              );
+
+              return (
+                <div className="flex items-center gap-2">
+                  <button
+                    className="bg-white border text-sm border-black rounded px-2 py-1 flex items-center font-mono hover:opacity-50"
+                    onClick={() => openChainModal()}
+                  >
+                    {chain.name}
+                    <SwitchHorizontalIcon className="h-4 w-4 ml-2" />
+                  </button>
+                  <button
+                    className="bg-white border text-sm border-black rounded px-2 py-1 flex items-center font-mono hover:opacity-50"
+                    onClick={() => openAccountModal()}
+                  >
+                    {account.ensName ? account.ensName : keyDetails}
+                    <FingerPrintIcon className="h-4 w-4 ml-2" />
+                  </button>
+                </div>
+              );
+            }}
+          </ConnectButton.Custom>
         </div>
-        <div className=""></div>
+        {/* <div className="">
+          <button className="bg-white border text-sm border-black rounded px-2 py-1 flex items-center">
+            Sell Something
+            <LightningBoltIcon className="h-4 w-4 ml-2" />
+          </button>
+        </div> */}
       </div>
       {props.children}
     </div>
@@ -108,6 +184,8 @@ export default function Page() {
   return (
     <Layout>
       <div className="h-full p-4">
+        <h1 className="font-serif text-2xl pb-2">Recent Sell Orders</h1>
+        <p className="pb-8">This list may be incomplete.</p>
         <Results />
       </div>
     </Layout>
