86510a360d42edda7baf49b6a3b787bd0b889132
Justin <justintorre75@gmail.com>
2022-05-30 18:11:42 -0700

    Kovan graph stuff

--- www/lib/useSubgraph.ts
+++ www/lib/useSubgraph.ts
@@ -4,6 +4,7 @@ import { useNetwork } from 'wagmi';
 
 const RINKEBY = 'https://api.thegraph.com/subgraphs/name/rwtp/rinkeby';
 const OPTIMISM = 'https://api.thegraph.com/subgraphs/name/rwtp/optimism';
+const KOVAN = 'https://api.thegraph.com/subgraphs/name/rwtp/kovan';
 
 const fetcher = (url: string, query: any, variables: any) =>
   request(url, query, variables);
@@ -14,6 +15,8 @@ export function useSubgraph<T>(args: string | [string, any]) {
   let chain = OPTIMISM;
   if (network.activeChain?.id === 4) {
     chain = RINKEBY;
+  } else if (network.activeChain?.id === 42) {
+    chain = KOVAN;
   }
 
   return useSWR<T>([chain, ...[args]].flat(), fetcher);
