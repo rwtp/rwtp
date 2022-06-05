0b3c99ff3f6432d8bd9cc77c9f2b25b997a6e01e
Justin <justintorre75@gmail.com>
2022-06-05 14:56:07 -0700

    order prettier

--- src/OrderBook.sol
+++ src/OrderBook.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import "@openzeppelin/contracts/security/Pausable.sol";
+import '@openzeppelin/contracts/security/Pausable.sol';
 import './Order.sol';
 import './interfaces/IOrderBook.sol';
 
