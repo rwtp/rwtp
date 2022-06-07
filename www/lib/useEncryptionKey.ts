18e9be7e6772f65df19f6106e06003ada42843f0
Justin <justintorre75@gmail.com>
2022-06-06 23:20:49 -0700

    Revert "changed useEffect -> useCallback"
    This reverts commit 18166066c5b76b28294827da570b6018944416b5.

--- www/lib/useEncryptionKey.ts
+++ www/lib/useEncryptionKey.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as nacl from 'tweetnacl';
 import { useKeystore } from './keystore';
 
@@ -12,25 +12,24 @@ export function useEncryptionKeypair() {
   const [encryptionKeypair, setEncryptionKeypair] =
     useState<nacl.BoxKeyPair | null>(null);
 
-  let memoizedCallback = useCallback(() => {
-    async function load(): Promise<any> {
+  // linear #REAL-411 - we should use a better hook here.
+  // see - https://stackoverflow.com/questions/55840294/how-to-fix-missing-dependency-warning-when-using-useeffect-react-hook
+  useEffect(() => {
+    async function load() {
       const result = await keystore.get(ENCRYPTION_KEY);
       if (!result) {
         let keypair = nacl.box.keyPair();
         let secretKey = Buffer.from(keypair.secretKey).toString('hex');
         await keystore.put(ENCRYPTION_KEY, secretKey);
-        return keypair;
-      } else {
-        const secretKey = Uint8Array.from(Buffer.from(result, 'hex'));
-        const keypair = nacl.box.keyPair.fromSecretKey(secretKey);
-        return keypair;
+        setEncryptionKeypair(keypair);
       }
+
+      const secretKey = Uint8Array.from(Buffer.from(result, 'hex'));
+      const keypair = nacl.box.keyPair.fromSecretKey(secretKey);
+      setEncryptionKeypair(keypair);
     }
-    return load();
-  }, [keystore]);
-  memoizedCallback().then((keypair) => {
-    setEncryptionKeypair(keypair);
-  });
+    load().catch(console.error);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (encryptionKeypair) {
     return {
