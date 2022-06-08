c869d5970023d447e0531aecb110ff77394699fe
Justin <justintorre75@gmail.com>
2022-06-07 19:14:35 -0700

    :)

--- examples/deploy_sell_order/index.ts
+++ examples/deploy_sell_order/index.ts
@@ -1,24 +1,8 @@
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { ethers } from 'ethers';
-import { OrderBook, SellOrder } from 'rwtp';
+import { OrderBook, Order } from 'rwtp';
 let GAS = 13300000;
 let WRAPPED_ETH_ADDRESS = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
-// let ORDER_BOOK_ADDRESS = "0x667D3DED6C891453E4b1BF6032CD0c22E0c31baC";
-let ORDER_BOOK_ADDRESS = "0xeEc309065c8Cfb99D1F6bBb97b139488F3A90d03";
-// Parse arguments
-const args = process.argv.slice(2);
-
-function invalidArgs(): void {
-  console.error('Usage: yarn start <RPC_URL>');
-  console.error('[--createSellOrder <SELLER_PRIVATE_KEY>]');
-  console.error("args: ", args);
-  console.error("args len: ", args.length);
-  process.exit(1);
-}
-
-if (args.length <= 2) {
-  invalidArgs();
-}
+let ORDER_BOOK_ADDRESS = "0xbd2e1dbe56053ee310249ce5969208ad7aa72dd0";
 
 function factory(contract: any, wallet: ethers.Wallet): ethers.ContractFactory {
   return new ethers.ContractFactory(contract.abi, contract.bytecode, wallet);
@@ -31,16 +15,14 @@ async function createOrderBook(wallet: ethers.Wallet): Promise<any> {
   return orderBook;
 }
 
-
-// Compatible with
-// - https://rinkeby.etherscan.io/token/0xc778417e063141139fce010982780140aa0cd5ab
-// - https://mumbai.polygonscan.com/token/0x9c3c9283d3e44854697cd22d3faa240cfb032889?a=0x20c9560d42566d6ead1da3dc84137e84838fa696#writeContract
 const ERC20_ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "guy", "type": "address" }, { "name": "wad", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "src", "type": "address" }, { "name": "dst", "type": "address" }, { "name": "wad", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "wad", "type": "uint256" }], "name": "withdraw", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "dst", "type": "address" }, { "name": "wad", "type": "uint256" }], "name": "transfer", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [], "name": "deposit", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }, { "name": "", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "src", "type": "address" }, { "indexed": true, "name": "guy", "type": "address" }, { "indexed": false, "name": "wad", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "src", "type": "address" }, { "indexed": true, "name": "dst", "type": "address" }, { "indexed": false, "name": "wad", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "dst", "type": "address" }, { "indexed": false, "name": "wad", "type": "uint256" }], "name": "Deposit", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "src", "type": "address" }, { "indexed": false, "name": "wad", "type": "uint256" }], "name": "Withdrawal", "type": "event" }];
 function purchasingToken(purchaseTokenAddress: string, wallet: ethers.Wallet): ethers.Contract {
   const purchasingToken = new ethers.Contract(purchaseTokenAddress, ERC20_ABI, wallet);
   return purchasingToken.attach(purchaseTokenAddress);
 }
 
+const MAKER_PRIVATE_KEY="0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d";
+const PROVIDER="https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
 const CREATE_ORDER = false;
 const SET_ACTIVE = true;
 const BUY_SEQUENCE = false;
@@ -48,9 +30,31 @@ const COMMIT_ORDER = false;
 const CONFIRM_OFFER = false;
 // MAIN FUNCTION
 (async function () {
+  // Create a wallet
+  const provider = new ethers.providers.JsonRpcProvider(PROVIDER);
+  const maker_wallet = new ethers.Wallet(MAKER_PRIVATE_KEY, provider);
+  console.log('Wallet address:', maker_wallet.address);
+
+  let orderBook = new ethers.Contract(ORDER_BOOK_ADDRESS, OrderBook.abi, maker_wallet);
+  console.log("orderBook.address: ", orderBook.address);
+  console.log("orderBook: ", orderBook);
+
+  return;
+  let sellOrder = await orderBook.createOrder(
+    maker_wallet.address,
+    WRAPPED_ETH_ADDRESS,
+    10,
+    "ipfs://QmZHqdtFg9rLh7fcxPGMKfVn3WxeTKNrv5WzAPZAv86Jb3",
+    1000,
+    { gasLimit: GAS }
+  )
+
+
+
+  const args = [];
   console.log("Running");
   let rpc_url = args[0];
-  const provider = new ethers.providers.JsonRpcProvider(rpc_url);
+  
   let command = args[1];
   console.log("Command: ", command);
   if (command === "--createSellOrder" && args.length === 4) {
@@ -67,7 +71,7 @@ const CONFIRM_OFFER = false;
 
 
     // Comment this if you want to create a new order book 
-    let orderBook = new ethers.Contract(ORDER_BOOK_ADDRESS, OrderBook.abi, sellerWallet);
+    
 
     // Uncomment this if you want to create a new order book
     // let orderBook = await createOrderBook(sellerWallet);
@@ -102,10 +106,10 @@ const CONFIRM_OFFER = false;
     let buyerBalance = await buyerWallet.getBalance();
     console.log('Buyer Balance:', ethers.utils.formatEther(buyerBalance), 'ETH\n');
 
-    let sellOrderContractBuyer = new ethers.Contract(sellOrderAddress, SellOrder.abi, buyerWallet);
+    let sellOrderContractBuyer = new ethers.Contract(sellOrderAddress, Order.abi, buyerWallet);
 
     if (SET_ACTIVE) {
-      let sellOrderContractSeller = new ethers.Contract(sellOrderAddress, SellOrder.abi, sellerWallet);
+      let sellOrderContractSeller = new ethers.Contract(sellOrderAddress, Order.abi, sellerWallet);
       console.log(sellOrderContractSeller);
       // return;
       let setActive = await sellOrderContractSeller.setActive(false, { gasLimit: GAS });
@@ -153,7 +157,7 @@ const CONFIRM_OFFER = false;
     }
 
 
-    let sellOrderContractSeller = new ethers.Contract(sellOrderAddress, SellOrder.abi, sellerWallet);
+    let sellOrderContractSeller = new ethers.Contract(sellOrderAddress, Order.abi, sellerWallet);
     let offer = await sellOrderContractSeller.offers(buyerWallet.address, 0);
     console.log("offer state: ", offer.state);
     if (COMMIT_ORDER) {
@@ -206,7 +210,6 @@ const CONFIRM_OFFER = false;
 
   } else {
     console.log("Invalid command", args[2]);
-    invalidArgs();
   }
 
 })().catch(console.error);
