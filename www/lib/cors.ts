4ee5d5f009cc338bc3fd2aced79cfd73be0d07a0
Evan Conrad <evan@roomservice.dev>
2022-06-15 14:24:27 -0700

    add default cors options

--- www/lib/cors.ts
+++ www/lib/cors.ts
@@ -17,9 +17,5 @@ function initMiddleware(middleware: any) {
 // Initialize the cors middleware
 export const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-    origin: '*',
-  })
+  Cors()
 );
